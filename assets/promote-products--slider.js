07c210b5c787aa8e484bc834748441d2f9ef990f
Vitaliy Dmytruk <vitaliy@swissuplabs.com>
2023-04-07 22:01:28 +0300

    Slider button and scrollbar

--- assets/promote-products--slider.js
+++ assets/promote-products--slider.js
@@ -3,6 +3,39 @@ if (typeof PromoteProductsSlider !== 'function') {
     constructor() {
       super();
       this.list = this.querySelector('.promote-products--list');
+      this.buttonNext = this.querySelector('[name=next]');
+      this.buttonPrev = this.querySelector('[name=prev]');
+
+      this.toggleActionsVisibility();
+      const resizeObserver = new ResizeObserver(this.toggleActionsVisibility.bind(this));
+      resizeObserver.observe(this.list);
+      this.buttonNext.addEventListener('click', this.onButtonClick.bind(this));
+      this.buttonPrev.addEventListener('click', this.onButtonClick.bind(this));
+    }
+
+    toggleActionsVisibility() {
+      if ((this.list.scrollWidth - this.list.clientWidth) > 0) {
+        this.buttonNext.hidden = false;
+        this.buttonPrev.hidden = false;
+      } else {
+        this.buttonNext.hidden = true;
+        this.buttonPrev.hidden = true;
+      }
+    }
+
+    onButtonClick(event) {
+      const button = event.currentTarget;
+      const step = button.dataset.step || 1;
+      const itemOffset = this.list.children[0].clientWidth;
+
+      event.preventDefault();
+      this.listScrollPosition =
+        button.name === 'next'
+          ? this.list.scrollLeft + step * itemOffset
+          : this.list.scrollLeft - step * itemOffset;
+      this.list.scrollTo({
+        left: this.listScrollPosition,
+      });
     }
   }
   
